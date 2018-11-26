869f838b3cb0d92d0a13730363f2f64f7082c867
叶信节 <yexinjie@souche.com>
2018-11-26 18:15:45 +0800

    Add sb rabbitmq test

---
+++ cloud-alibaba/src/main/java/com/helloxin/cloud/alibaba/App.java
@@ -0,0 +1,13 @@
+package com.helloxin.cloud.alibaba;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ rabbitmq/src/test/java/com/helloxin/rabbitmq/HelloApplicationTests.java
@@ -0,0 +1,23 @@
+package com.helloxin.rabbitmq;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.helloxin.rabbitmq.message.Sender;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = HelloApplication.class)
+public class HelloApplicationTests {
+
+	@Autowired
+	private Sender sender;
+
+	@Test
+	public void hello() throws Exception {
+		sender.send();
+	}
+
+}
