2c9fd047f3d1a28665b3bc61e67ec3375576b1df
叶信节 <yexinjie@souche.com>
2019-09-24 16:41:37 +0800

    ADD  spring batch demo

---
+++ spring-batch/src/main/java/com/helloxin/spring/batch/Processor.java
@@ -0,0 +1,12 @@
+package com.helloxin.spring.batch;
+
+import org.springframework.batch.item.ItemProcessor;
+
+public class Processor implements ItemProcessor<String, String> {
+    
+    @Override
+    public String process(String data) throws Exception {
+        return data.toUpperCase();
+    }
+    
+}
