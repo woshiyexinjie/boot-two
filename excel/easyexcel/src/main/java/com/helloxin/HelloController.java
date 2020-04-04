27b9c158b6402a58a601c9f0b78795978336f519
叶信节 <yexinjie@souche.com>
2020-04-04 16:26:25 +0800

    ADD  在response中导出 多sheet excel

--- excel/easyexcel/src/main/java/com/helloxin/HelloController.java
+++ excel/easyexcel/src/main/java/com/helloxin/HelloController.java
@@ -1,6 +1,8 @@
 package com.helloxin;
 
 import com.alibaba.excel.EasyExcel;
+import com.alibaba.excel.ExcelWriter;
+import com.alibaba.excel.write.metadata.WriteSheet;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -37,6 +39,41 @@ public class HelloController {
         reportCig.addAll(insertIndex, customCig);
         EasyExcel.write(response.getOutputStream()).head(head(reportCig)).sheet("模板").doWrite(finaldata());
     }
+
+    @GetMapping("mulDownload")
+    public void mulDownload(HttpServletResponse response) throws IOException {
+        // 这里注意 有同学反应使用swagger 会导致各种问题，请直接用浏览器或者用postman
+        response.setContentType("application/vnd.ms-excel");
+        response.setCharacterEncoding("utf-8");
+        // 这里URLEncoder.encode可以防止中文乱码 当然和easyexcel没有关系
+        String fileName = URLEncoder.encode("测试", "UTF-8");
+        response.setHeader("Content-disposition", "attachment;filename=" + fileName + ".xlsx");
+        // 去调用写入,这里我调用了五次，实际使用时根据据库分页的总的页数来。这里最终会写到5个sheet里面
+
+        ExcelWriter excelWriter = EasyExcel.write(response.getOutputStream()).build();
+        for (int i = 0; i < 5; i++) {
+//             每次都要创建writeSheet 这里注意必须指定sheetNo 而且sheetName必须不一样。这里注意DemoData.clas 可以每次都变，我这里为了方便 所以用的同一个class 实际上可以一直变
+            WriteSheet writeSheet = EasyExcel.writerSheet(i, "模板" + i).head(DemoData.class).build();
+            // 分页去数据库查询数据 这里可以去数据库查询每一页的数据
+            List<DemoData> data = dataDemo();
+            excelWriter.write(data, writeSheet);
+        }
+        excelWriter.finish();
+
+    }
+
+    private List<DemoData> dataDemo() {
+        List<DemoData> list = new ArrayList<DemoData>();
+        for (int i = 0; i < 10; i++) {
+            DemoData data = new DemoData();
+            data.setString("字符串" + i);
+            data.setDate(new Date());
+            data.setDoubleData(0.56);
+            list.add(data);
+        }
+        return list;
+    }
+
 
     private List<List<String>> head(List<ExcelCell> finalExcelCig) {
         List<List<String>> list = new ArrayList<List<String>>();
--- excel/easyexcel/src/test/java/com/helloxin/EasyExcelBaseTest.java
+++ excel/easyexcel/src/test/java/com/helloxin/EasyExcelBaseTest.java
@@ -1,6 +1,8 @@
 package com.helloxin;
 
 import com.alibaba.excel.EasyExcel;
+import com.alibaba.excel.ExcelWriter;
+import com.alibaba.excel.write.metadata.WriteSheet;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -10,20 +12,66 @@ import java.util.List;
 /**
  * Unit test for simple App.
  */
-public class EasyExcelBaseTest
-{
+public class EasyExcelBaseTest {
     /**
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-     // 写法1
+    public void shouldAnswerWithTrue() {
+        // 写法1
         String fileName = "simpleWrite" + System.currentTimeMillis() + ".xlsx";
         // 这里 需要指定写用哪个class去写，然后写到第一个sheet，名字为模板 然后文件流会自动关闭
         // 如果这里想使用03 则 传入excelType参数即可
         EasyExcel.write(fileName, DemoData.class).sheet("模板").doWrite(data());
 
+    }
+
+    @Test
+    public void repeatedWrite() {
+        // 方法1 如果写到同一个sheet
+        String fileName = "simpleWrite" + System.currentTimeMillis() + ".xlsx";
+        // 这里 需要指定写用哪个class去写
+        ExcelWriter excelWriter = EasyExcel.write(fileName, DemoData.class).build();
+        // 这里注意 如果同一个sheet只要创建一次
+        WriteSheet writeSheet = EasyExcel.writerSheet("模板").build();
+        // 去调用写入,这里我调用了五次，实际使用时根据数据库分页的总的页数来
+        for (int i = 0; i < 5; i++) {
+            // 分页去数据库查询数据 这里可以去数据库查询每一页的数据
+            List<DemoData> data = data();
+            excelWriter.write(data, writeSheet);
+        }
+        /// 千万别忘记finish 会帮忙关闭流
+        excelWriter.finish();
+
+        // 方法2 如果写到不同的sheet 同一个对象
+        fileName = "simpleWrite" + System.currentTimeMillis() + ".xlsx";
+        // 这里 指定文件
+        excelWriter = EasyExcel.write(fileName, DemoData.class).build();
+        // 去调用写入,这里我调用了五次，实际使用时根据数据库分页的总的页数来。这里最终会写到5个sheet里面
+        for (int i = 0; i < 5; i++) {
+            // 每次都要创建writeSheet 这里注意必须指定sheetNo 而且sheetName必须不一样
+            writeSheet = EasyExcel.writerSheet(i, "模板" + i).build();
+            // 分页去数据库查询数据 这里可以去数据库查询每一页的数据
+            List<DemoData> data = data();
+            excelWriter.write(data, writeSheet);
+        }
+        /// 千万别忘记finish 会帮忙关闭流
+        excelWriter.finish();
+
+        // 方法3 如果写到不同的sheet 不同的对象
+        fileName = "simpleWrite" + System.currentTimeMillis() + ".xlsx";
+        // 这里 指定文件
+        excelWriter = EasyExcel.write(fileName).build();
+        // 去调用写入,这里我调用了五次，实际使用时根据数据库分页的总的页数来。这里最终会写到5个sheet里面
+        for (int i = 0; i < 5; i++) {
+            // 每次都要创建writeSheet 这里注意必须指定sheetNo 而且sheetName必须不一样。这里注意DemoData.class 可以每次都变，我这里为了方便 所以用的同一个class 实际上可以一直变
+            writeSheet = EasyExcel.writerSheet(i, "模板" + i).head(DemoData.class).build();
+            // 分页去数据库查询数据 这里可以去数据库查询每一页的数据
+            List<DemoData> data = data();
+            excelWriter.write(data, writeSheet);
+        }
+        /// 千万别忘记finish 会帮忙关闭流
+        excelWriter.finish();
     }
 
     private List<DemoData> data() {
