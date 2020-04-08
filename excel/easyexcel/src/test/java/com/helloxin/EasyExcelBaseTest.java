085eeb783645c1626ef0419966081dd2b348bb71
叶信节 <yexinjie@souche.com>
2020-04-08 11:25:48 +0800

    ADD easyexcel 不使用默认样式

--- excel/easyexcel/src/test/java/com/helloxin/EasyExcelBaseTest.java
+++ excel/easyexcel/src/test/java/com/helloxin/EasyExcelBaseTest.java
@@ -27,6 +27,15 @@ public class EasyExcelBaseTest {
     }
 
     @Test
+    public void should_get_excel_no_style_success() {
+        // 写法1
+        String fileName = "simpleWrite" + System.currentTimeMillis() + ".xlsx";
+        // 这里 需要指定写用哪个class去写，然后写到第一个sheet，名字为模板 然后文件流会自动关闭
+        // 如果这里想使用03 则 传入excelType参数即可
+        EasyExcel.write(fileName, DemoData.class).useDefaultStyle(false).sheet("模板").doWrite(data());
+    }
+
+    @Test
     public void repeatedWrite() {
         // 方法1 如果写到同一个sheet
         String fileName = "simpleWrite" + System.currentTimeMillis() + ".xlsx";
