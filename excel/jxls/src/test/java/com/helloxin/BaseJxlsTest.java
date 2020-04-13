f5de7253fedd41d9fd670eb8e9fb22f5566e3b2a
叶信节 <yexinjie@souche.com>
2020-04-13 11:36:43 +0800

    DELETE 合并的module

--- excel/jxls/src/test/java/com/helloxin/BaseJxlsTest.java
+++ excel/jxls/src/test/java/com/helloxin/BaseJxlsTest.java
@@ -3,19 +3,10 @@ package com.helloxin;
 import com.helloxin.domain.Employee;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.util.Lists;
-import org.junit.Test;
-import org.jxls.common.Context;
-import org.jxls.util.JxlsHelper;
 
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.math.BigDecimal;
 import java.util.Date;
 import java.util.List;
-
-import static com.sun.org.apache.xalan.internal.utils.SecuritySupport.getResourceAsStream;
 
 @Slf4j
 public class BaseJxlsTest
@@ -23,20 +14,20 @@ public class BaseJxlsTest
     /**
      * Rigorous Test :-)
      */
-    @Test
-    public void shouldAnswerWithTrue() throws IOException {
-        log.info("Running Object Collection demo");
-        List<Employee> employees = generateSampleEmployeeData();
-        try(InputStream is = getResourceAsStream("object_collection_template.xls")) {
-            try (OutputStream os = new FileOutputStream("target/object_collection_output.xls")) {
-                Context context = new Context();
-                context.putVar("employees", employees);
-//                JxlsHelper.getInstance().processTemplate(is, os, context);
-                JxlsHelper.getInstance().processTemplateAtCell(is, os, context, "Result!A1");
-
-            }
-        }
-    }
+//    @Test
+//    public void shouldAnswerWithTrue() throws IOException {
+//        log.info("Running Object Collection demo");
+//        List<Employee> employees = generateSampleEmployeeData();
+//        try(InputStream is = getResourceAsStream("object_collection_template.xls")) {
+//            try (OutputStream os = new FileOutputStream("target/object_collection_output.xls")) {
+//                Context context = new Context();
+//                context.putVar("employees", employees);
+////                JxlsHelper.getInstance().processTemplate(is, os, context);
+//                JxlsHelper.getInstance().processTemplateAtCell(is, os, context, "Result!A1");
+//
+//            }
+//        }
+//    }
 
     private List<Employee> generateSampleEmployeeData() {
         Employee employee = new Employee();
--- excel/jxls/src/test/java/com/helloxin/XmlBuilderOutputTest.java
+++ excel/jxls/src/test/java/com/helloxin/XmlBuilderOutputTest.java
@@ -3,24 +3,10 @@ package com.helloxin;
 import com.helloxin.domain.Employee;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.util.Lists;
-import org.junit.Test;
-import org.jxls.area.Area;
-import org.jxls.builder.AreaBuilder;
-import org.jxls.builder.xml.XmlAreaBuilder;
-import org.jxls.common.CellRef;
-import org.jxls.common.Context;
-import org.jxls.transform.Transformer;
-import org.jxls.util.TransformerFactory;
 
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.math.BigDecimal;
 import java.util.Date;
 import java.util.List;
-
-import static com.sun.org.apache.xalan.internal.utils.SecuritySupport.getResourceAsStream;
 
 @Slf4j
 public class XmlBuilderOutputTest
@@ -28,24 +14,24 @@ public class XmlBuilderOutputTest
     /**
      * Rigorous Test :-)
      */
-    @Test
-    public void shouldAnswerWithTrue() throws IOException {
-        List<Employee> employees = generateSampleEmployeeData();
-        try(InputStream is = getResourceAsStream("object_collection_xmlbuilder_template.xls")) {
-            try (OutputStream os = new FileOutputStream("target/object_collection_xmlbuilder_output.xls")) {
-                Transformer transformer = TransformerFactory.createTransformer(is, os);
-                try (InputStream configInputStream = getResourceAsStream("object_collection_xmlbuilder.xml")) {
-                    AreaBuilder areaBuilder = new XmlAreaBuilder(configInputStream, transformer);
-                    List<Area> xlsAreaList = areaBuilder.build();
-                    Area xlsArea = xlsAreaList.get(0);
-                    Context context = new Context();
-                    context.putVar("employees", employees);
-                    xlsArea.applyAt(new CellRef("Result!A1"), context);
-                    transformer.write();
-                }
-            }
-        }
-    }
+//    @Test
+//    public void shouldAnswerWithTrue() throws IOException {
+//        List<Employee> employees = generateSampleEmployeeData();
+//        try(InputStream is = getResourceAsStream("object_collection_xmlbuilder_template.xls")) {
+//            try (OutputStream os = new FileOutputStream("target/object_collection_xmlbuilder_output.xls")) {
+//                Transformer transformer = TransformerFactory.createTransformer(is, os);
+//                try (InputStream configInputStream = getResourceAsStream("object_collection_xmlbuilder.xml")) {
+//                    AreaBuilder areaBuilder = new XmlAreaBuilder(configInputStream, transformer);
+//                    List<Area> xlsAreaList = areaBuilder.build();
+//                    Area xlsArea = xlsAreaList.get(0);
+//                    Context context = new Context();
+//                    context.putVar("employees", employees);
+//                    xlsArea.applyAt(new CellRef("Result!A1"), context);
+//                    transformer.write();
+//                }
+//            }
+//        }
+//    }
 
     private List<Employee> generateSampleEmployeeData() {
         Employee employee = new Employee();
