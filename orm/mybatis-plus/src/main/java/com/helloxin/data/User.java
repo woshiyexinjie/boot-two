4d7fbc79e1329a3b5dd411095e3c8e321b60c7b7
叶信节 <yexinjie@souche.com>
2020-06-29 11:00:37 +0800

    ADD mybatis-plus test demo

---
+++ orm/mybatis-plus/src/main/java/com/helloxin/data/User.java
@@ -0,0 +1,11 @@
+package com.helloxin.data;
+
+import lombok.Data;
+
+@Data
+public class User {
+    private Long id;
+    private String name;
+    private Integer age;
+    private String email;
+}
---
+++ orm/mybatis-plus/src/main/java/com/helloxin/mapper/UserMapper.java
@@ -0,0 +1,7 @@
+package com.helloxin.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.helloxin.data.User;
+
+public interface UserMapper extends BaseMapper<User> {
+}
