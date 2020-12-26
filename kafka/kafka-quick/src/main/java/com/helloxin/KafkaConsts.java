40de54b92d8a86da4589dc2e2a19b8728a9d3c98
叶信节 <yexinjie@souche.com>
2020-12-27 00:51:40 +0800

    add kafka demo

---
+++ kafka/kafka-quick/src/main/java/com/helloxin/KafkaConsts.java
@@ -0,0 +1,16 @@
+package com.helloxin;
+
+/**
+ * Created by yebanxian on 2020/12/26.
+ */
+public interface KafkaConsts {
+    /**
+     * 默认分区大小
+     */
+    Integer DEFAULT_PARTITION_NUM = 3;
+
+    /**
+     * Topic 名称
+     */
+    String TOPIC_TEST = "test_xin";
+}
